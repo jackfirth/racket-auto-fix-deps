f1db0c1bf3d7e7de0bffe91693103627d22f5651
Jack Firth <jackhfirth@gmail.com>
2015-09-08 03:19:26 -0700

    Add git user name/email config

--- src/fix-deps.sh
+++ src/fix-deps.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+git config --global user.email "$GITHUB_USER_EMAIL"
+git config --global user.name "$GITHUB_USER_NAME"
+
 raco pkg install --deps search-auto --no-setup --clone $1
 cd $1
 /src/hub fork || echo "fork already exists"
